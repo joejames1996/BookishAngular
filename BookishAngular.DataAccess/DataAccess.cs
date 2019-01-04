64b4fb4b2e0ac58f93fa7275b1853f34205e14a5
joejames1996 <Joe.James@softwire.com>
2019-01-04 15:46:47 +0000

    Initial commit

---
+++ BookishAngular.DataAccess/DataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookishAngular.DataAccess
+{
+    public class DataAccess
+    {
+
+    }
+}
